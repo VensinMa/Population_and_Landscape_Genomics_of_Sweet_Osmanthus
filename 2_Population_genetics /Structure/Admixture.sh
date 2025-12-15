f36c97ad6510091bec32ed41c33fafb3593b7a88
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-12-15 22:15:16 +0800

    Update Admixture.sh

--- 2_Population_genetics /Structure/Admixture.sh
+++ 2_Population_genetics /Structure/Admixture.sh
@@ -7,7 +7,7 @@ cd /home/vensin/workspace/snpcalling_wild/12.population_genetics/admixture/resul
 # --cv: 开启交叉验证 (用于通过 CV error 选择最佳 K)
 # -s 123: 设置随机种子 (保证结果可复现)
 # > log_K{}.out: 将包含 CV error 的日志重定向保存
-seq 20 -1 2 | parallel -j 10 "admixture --cv -s 123 /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.LD.pruned.bed {} > log_K{}.out" &
+seq 2 20 | parallel -j 10 "admixture --cv -s 123 /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.LD.pruned.bed {} > log_K{}.out" &
 
 # 4. 确定最佳 K 值（CV error (Cross-Validation Error) 最小）
 grep -h "CV" log_K*.out | sort -n -k 4
