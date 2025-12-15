0c6a7802c416504bd96c8be8a45963e674442e57
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-12-15 22:14:56 +0800

    Update fastStructure.sh

--- 2_Population_genetics /Structure/fastStructure.sh
+++ 2_Population_genetics /Structure/fastStructure.sh
@@ -4,7 +4,7 @@ cd /home/vensin/workspace/snpcalling_wild/12.population_genetics/faststructure
 
 # 2. 倒序运行 fastStructure (从 K=20 到 K=2)
 # 使用 seq 20 -1 2
-seq 20 -1 2 | parallel -j 10 "structure.py -K {} \
+seq 2 20 | parallel -j 10 "structure.py -K {} \
     --input=/home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.LD.pruned \
     --output=/home/vensin/workspace/snpcalling_wild/12.population_genetics/faststructure/result/LD_faststructure_K_{} \
     --cv=5 --prior=simple --seed=123 \
