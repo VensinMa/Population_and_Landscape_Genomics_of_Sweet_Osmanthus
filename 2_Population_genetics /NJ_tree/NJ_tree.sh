4f4b4f912b4491b9970b80db520ca9a4f78c6c1a
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-12-15 20:01:00 +0800

    Update and rename 2_Population_genetics /NJ_tree.sh to 2_Population_genetics /NJ_tree/NJ_tree.sh

---
+++ 2_Population_genetics /NJ_tree/NJ_tree.sh
@@ -0,0 +1,5 @@
+
+cd /home/vensin/workspace/snpcalling_wild/12.population_genetics/208samples_tree
+vcf2dis_bootstrap_nj_tree_parallel.sh /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/208_samples_snp_filtered.LD.pruned.recode.vcf.gz 1000 28
+
+
