0ffdba063a0e5cd1d81ee2323115f650000855bc
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-12-15 20:01:17 +0800

    Update NJ_tree.sh to change working directory

--- 2_Population_genetics /NJ_tree/NJ_tree.sh
+++ 2_Population_genetics /NJ_tree/NJ_tree.sh
@@ -1,4 +1,3 @@
-
 cd /home/vensin/workspace/snpcalling_wild/12.population_genetics/208samples_tree
 vcf2dis_bootstrap_nj_tree_parallel.sh /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/208_samples_snp_filtered.LD.pruned.recode.vcf.gz 1000 28
 
