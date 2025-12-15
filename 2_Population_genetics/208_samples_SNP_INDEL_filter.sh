45aa76594c6669f55f6abfe0084cafd22b344ec7
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-12-15 15:25:49 +0800

    Add SNP and INDEL filtering script

---
+++ 2_Population_genetics/208_samples_SNP_INDEL_filter.sh
@@ -0,0 +1,36 @@
+################################################################ SNP ################################################################
+# 1. 创建输出目录
+mkdir -p /home/vensin/workspace/snpcalling_wild/11.Vcftools_VariantFiltration/snp
+cd /home/vensin/workspace/snpcalling_wild/11.Vcftools_VariantFiltration/snp
+
+# 2. 运行 VCFtools (SNP)
+# 压缩并建立索引 (生成 .vcf.gz 和 .tbi)
+vcftools --gzvcf /home/data/10.gatk_variantfiltration/SNP/snp_filtered.vcf.gz \
+  --min-alleles 2 --max-alleles 2 \
+  --minGQ 10 --minQ 30 --min-meanDP 6 \
+  --max-missing 0.9 --maf 0.05 \
+  --recode --recode-INFO-all \
+  --stdout \
+  | bgzip -@ 8 > /home/vensin/workspace/snpcalling_wild/11.Vcftools_VariantFiltration/snp/208_samples_snp_filtered.recode.vcf.gz
+
+# 然后建立索引
+tabix -p vcf /home/vensin/workspace/snpcalling_wild/11.Vcftools_VariantFiltration/snp/208_samples_snp_filtered.recode.vcf.gz
+
+################################################################ INDEL ##############################################################
+# 1. 创建并进入目录
+mkdir -p /home/vensin/workspace/snpcalling_wild/11.Vcftools_VariantFiltration/indel
+cd /home/vensin/workspace/snpcalling_wild/11.Vcftools_VariantFiltration/indel
+
+# 2. 运行 VCFtools (INDEL) -> 管道 -> bgzip 多线程压缩
+# 输入文件路径假设为: .../INDEL/indel_filtered.vcf.gz
+# 参数调整: 去掉 --maf, max-missing 改为 0.8
+vcftools --gzvcf /home/data/10.gatk_variantfiltration/INDEL/indel_filtered.vcf.gz \
+  --min-alleles 2 --max-alleles 2 \
+  --minGQ 10 --minQ 30 --min-meanDP 6 \
+  --max-missing 0.8 \
+  --recode --recode-INFO-all \
+  --stdout \
+  | bgzip -@ 8 > 208_samples_indel_filtered.recode.vcf.gz
+
+# 3. 建立索引
+tabix -p vcf 208_samples_indel_filtered.recode.vcf.gz
