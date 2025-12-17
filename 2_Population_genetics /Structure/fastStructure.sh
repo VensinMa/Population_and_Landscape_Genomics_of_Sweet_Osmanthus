356af8a08eb830669d0f5847033fe3e774ce34a7
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-12-17 15:32:20 +0800

    Update fastStructure.sh

--- 2_Population_genetics /Structure/fastStructure.sh
+++ 2_Population_genetics /Structure/fastStructure.sh
@@ -9,7 +9,19 @@ seq 2 20 | parallel -j 10 "structure.py -K {} --input=/home/vensin/workspace/snp
 
 # 等上面所有任务跑完后运行    
 cd /home/vensin/workspace/snpcalling_wild/12.population_genetics/faststructure/result
-chooseK.py --input=/home/vensin/workspace/snpcalling_wild/12.population_genetics/faststructure/result/LD_faststructure_K
+
+# 在 result 目录下运行
+for k in $(seq 2 20); do
+    ln -s LD_faststructure_K_${k}.${k}.log fs_combined.${k}.log
+done
+
+for k in $(seq 2 20); do
+    ln -s LD_faststructure_K_${k}.${k}.meanQ fs_combined.${k}.meanQ
+done
+
+#chooseK.py --input=/home/vensin/workspace/snpcalling_wild/12.population_genetics/faststructure/result/LD_faststructure_K
+chooseK.py --input=fs_combined
+
 ## Model complexity that maximizes marginal likelihood = 4
 ## Model components used to explain structure in data = 7
 
