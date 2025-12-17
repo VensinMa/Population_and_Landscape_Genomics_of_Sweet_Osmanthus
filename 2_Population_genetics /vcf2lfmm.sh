80df6321f1b076b9a83312f25b3e778cd27b9d25
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-12-17 21:26:06 +0800

    Update vcf2lfmm.sh

--- 2_Population_genetics /vcf2lfmm.sh
+++ 2_Population_genetics /vcf2lfmm.sh
@@ -1,4 +1,4 @@
-##################################### ALL  ######################################
+##################################### ALL SNP  ######################################
 # Step 1: VCF -> .raw, VCF 转 Raw(0, 1, 2 的数字编码)
 plink --vcf /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.recode.vcf.gz \
       --recodeA --allow-extra-chr \
@@ -8,11 +8,10 @@ plink --vcf /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_sa
 sed '1d; s/NA/9/g' /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.plink.recodeA.raw | \
 awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.plink.recodeA.lfmm
 
-plink --vcf /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.recode.vcf.gz \
-      --recode --allow-extra-chr \
-      --out /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.plink
+# 
+bcftools query -f '%CHROM:%POS\n' /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.recode.vcf.gz > /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.ID
 
-##################################### LD  ######################################
+##################################### LD SNP ######################################
 # Step 1: VCF -> .raw, VCF 转 Raw(0, 1, 2 的数字编码)
 plink --vcf /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.LD.pruned.recode.vcf.gz \
       --recodeA --allow-extra-chr \
@@ -21,3 +20,20 @@ plink --vcf /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_sa
 # Step 2: Raw -> LFMM
 sed '1d; s/NA/9/g' /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.LD.pruned.plink.recodeA.raw | \
 awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/snp/202_samples_snp_filtered.LD.pruned.plink.recodeA.lfmm
+
+
+##################################### ALL INDEL ######################################
+# Step 1: VCF -> .raw, VCF 转 Raw(0, 1, 2 的数字编码)
+plink --vcf /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/indel/202_samples_indel_filtered.recode.vcf.gz \
+      --recodeA --allow-extra-chr \
+      --out /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/indel/202_samples_indel_filtered.plink.recodeA
+
+# Step 2: Raw -> LFMM
+sed '1d; s/NA/9/g' /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/indel/202_samples_indel_filtered.plink.recodeA.raw | \
+awk '{ $1=$2=$3=$4=$5=$6=""; print substr($0, index($0,$7)) }' > /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/indel/202_samples_indel_filtered.plink.recodeA.lfmm
+
+bcftools query -f '%CHROM:%POS\n' /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/indel/202_samples_indel_filtered.recode.vcf.gz > /home/vensin/workspace/snpcalling_wild/11.vcftools_filter/indel/202_samples_indel_filtered.ID
+
+
+
+      
