1ff7532f5c957077fabcd4a6efaf1fff98a5db0b
Ma Wenxin <62129132+VensinMa@users.noreply.github.com>
2025-12-25 15:07:02 +0800

    Update easySFS.sh

--- 2_Population_genetics /DemographicHistory/easySFS.sh
+++ 2_Population_genetics /DemographicHistory/easySFS.sh
@@ -3,12 +3,13 @@ conda create -n easySFS && conda activate easySFS
 git clone https://github.com/isaacovercast/easySFS.git
 
 cd /home/vensin/workspace/snpcalling_wild/12.population_genetics/Demographic_History/easySFS
-easySFS.py -i /home/vensin/workspace/snpcalling_wild/13.genetic_load/est-sfs/205_samples_snp_filtered.nomissing.recode_polarized.vcf -p 205samples.pop -a --preview
+easySFS.py -i /home/vensin/workspace/snpcalling_wild/13.genetic_load/est-sfs/205_samples_snp_filtered.nomissing.recode_polarized.vcf -p 205samples.pop -a --unfolded --preview
 # -a 表示生成 Unfolded SFS (适用于已极化的数据)
 # --proj 顺序必须严格对应: East, Central, SW-Yunnan, SW-Guizhou, OUTGROUP
 
 easySFS.py -i /home/vensin/workspace/snpcalling_wild/13.genetic_load/est-sfs/205_samples_snp_filtered.nomissing.recode_polarized.vcf \
 -p 205samples.pop \
 -a \
+--unfolded\
 --proj 272,54,24,54,6 \
 -o output_fitcoal_unfolded
